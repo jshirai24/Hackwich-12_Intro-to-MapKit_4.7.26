fa31ab75083a2af1830c6df2e6aadbb8206af89d
Jillian Shirai <jtshirai@hawaii.edu>
2026-04-07 11:52:31 -1000

    Sucessfully centered map view around UHWO campus

--- Hackwich 12_Intro to MapKit_4.7.26/ViewController.swift
+++ Hackwich 12_Intro to MapKit_4.7.26/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    //inital location
+    let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
+    let regionRadius: CLLocationDistance = 1000
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centerMapOnLocation(location: initialLocation)
     }
 
-
+    func centerMapOnLocation (location: CLLocation)
+    {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    
+    }
+    
+    
+    
+    
 }
 
